9525613fc6ef3f0161937aca4d7e244293bc0d81
Fernando Cugik <fernando.cugik@gmail.com>
2014-09-15 00:47:55 +0100

    init.pp.cugik

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,6 +1,22 @@
 class puppet {
-  file { '/usr/local/bin/papply':
-    source => 'puppet:///modules/puppet/papply.sh',
-    mode => '0755',
-  }
+file { '/usr/local/bin/papply':
+source => 'puppet:///modules/puppet/papply.sh',
+mode => '0755',
+}
+file { '/usr/local/bin/pull-updates':
+source => 'puppet:///modules/puppet/pull-updates.sh',
+mode => '0755',
+}
+file { '/home/cugik/.ssh/id_rsa':
+source => 'puppet:///modules/puppet/cugik.priv',
+owner => 'cugik',
+mode => '0600',
+}
+cron { 'run-puppet':
+ensure => 'present',
+user => 'cugik',
+command => '/usr/local/bin/pull-updates',
+minute => '*/10',
+hour => '*',
+}
 }
